6e8e88f5d788f3bcb92d0af7271c5bdba2ef33ec
nosyradigital8-gif <nosyradigital8@gmail.com>
2025-12-16 12:51:21 +0100

    Add files via upload

---
+++ src/app/App.tsx
@@ -0,0 +1,23 @@
+import { Header } from './components/Header';
+import { Hero } from './components/Hero';
+import { About } from './components/About';
+import { Services } from './components/Services';
+import { Testimonials } from './components/Testimonials';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <Testimonials />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+}
